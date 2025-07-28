322d611a9d73e4a60d1a0688602fb43304810be1
shubhgit-hub <shubhamyada64@gmail.com>
2025-07-28 16:31:14 +0530

    first commit

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
+const API_KEY ="ae486be70f7d9c63f9ef45b20ab017d2" ;
 
 export default function Weather() {
   const [weather, setWeather] = useState(null);
@@ -10,7 +10,28 @@ export default function Weather() {
   const [loading, setLoading] = useState(false);
   const [error, setError]     = useState(null);
 
-  
+  useEffect(() => {
+    const fetchWeather = async () => {
+      try {
+        setLoading(true);
+        setError(null); // reset previous error
+        const response = await axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+        );
+        setWeather(response.data);
+      } catch (err) {
+        setError(" City not found or API error");
+        setWeather(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchWeather();
+  }, [city]);
+             
+        
+    
 
   const handleSubmit = (e) => {
     e.preventDefault();
